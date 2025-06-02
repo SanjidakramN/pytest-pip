13555254324ea9ba44b5d58ae5de6f02a1e82e30
SanjidakramN <sanjidakram2@gmail.com>
2025-06-02 09:55:20 +0000

    added

---
+++ vars/pyTest.groovy
@@ -0,0 +1,9 @@
+def call() {
+   sh '''
+      sudo apt-get update -y 
+      sudo apt-get install -y python3 python3-pip python3-venv
+      python3 -m venv myenv
+      . myenv/bin/activate
+      pip install --upgrade pip
+   '''
+
